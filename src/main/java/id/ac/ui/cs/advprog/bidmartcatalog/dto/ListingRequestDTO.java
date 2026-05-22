c3c163c0754e7571c0b53438c01ea19be15ccebc
unknown <seanmarcello836@gmail.com>
2026-05-22 20:22:14 +0700

    fix: resolve HTTP 400 on auction publish by fixing date format and adding required headers

--- src/main/java/id/ac/ui/cs/advprog/bidmartcatalog/dto/ListingRequestDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmartcatalog/dto/ListingRequestDTO.java
@@ -1,6 +1,8 @@
 package id.ac.ui.cs.advprog.bidmartcatalog.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -11,5 +13,7 @@ public class ListingRequestDTO {
     private Double startingPrice;
     private Double reservePrice;
     private UUID categoryId;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime endTime;
 }
